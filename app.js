83287e2dfe3286ab06cb9fb780e3b5f1459e1d2f
Thi anh thu Le <thle@c2r2p11.hive.fi>
2022-04-27 11:56:43 +0300

    change client_id and redirect_id

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-(function() {
-    
-    function login(callback) {
-        var CLIENT_ID = '6b284830006843e7ae7b170725715aed';
-        var REDIRECT_URI = 'http://jmperezperez.com/spotify-oauth-jsfiddle-proxy/';
-        function getLoginURL(scopes) {
-            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
-              '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
-              '&scope=' + encodeURIComponent(scopes.join(' ')) +
-              '&response_type=token';
-        }
-        
-        var url = getLoginURL([
-            'user-read-email'
-        ]);
-        
-        var width = 450,
-            height = 730,
-            left = (screen.width / 2) - (width / 2),
-            top = (screen.height / 2) - (height / 2);
-    
-        window.addEventListener("message", function(event) {
-            var hash = JSON.parse(event.data);
-            if (hash.type == 'access_token') {
-                callback(hash.access_token);
-            }
-        }, false);
-        
-        var w = window.open(url,
-                            'Spotify',
-                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                           );
-    }
+(function () {
 
-    function getUserData(accessToken) {
-        return $.ajax({
-            url: 'https://api.spotify.com/v1/me',
-            headers: {
-               'Authorization': 'Bearer ' + accessToken
-            }
-        });
+	function login(callback) {
+		var CLIENT_ID = '87204af108984c64ad5a9db47d995b63';
+		var REDIRECT_URI = 'http://127.0.0.1:5500/index.html';
+		function getLoginURL(scopes) {
+			return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+				'&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+				'&scope=' + encodeURIComponent(scopes.join(' ')) +
+				'&response_type=token';
+		}
+
+		var url = getLoginURL([
+			'user-read-email'
+		]);
+
+		var width = 450,
+			height = 730,
+			left = (screen.width / 2) - (width / 2),
+			top = (screen.height / 2) - (height / 2);
+
+		window.addEventListener("message", function (event) {
+			var hash = JSON.parse(event.data);
+			if (hash.type == 'access_token') {
+				callback(hash.access_token);
+			}
+		}, false);
+
+		var w = window.open(url,
+			'Spotify',
+			'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+		);
+	}
+
+	function getUserData(accessToken) {
+		return $.ajax({
+			url: 'https://api.spotify.com/v1/me',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		});
 		console.log("i was here");
-    }
+	}
 
-    var templateSource = document.getElementById('result-template').innerHTML,
-        template = Handlebars.compile(templateSource),
-        resultsPlaceholder = document.getElementById('result'),
-        loginButton = document.getElementById('btn-login');
-    
-    loginButton.addEventListener('click', function() {
-        login(function(accessToken) {
-            getUserData(accessToken)
-                .then(function(response) {
-                    loginButton.style.display = 'none';
-                    resultsPlaceholder.innerHTML = template(response);
-                });
-            });
-			console.log("i was here");
-    });
-    
+	var templateSource = document.getElementById('result-template').innerHTML,
+		template = Handlebars.compile(templateSource),
+		resultsPlaceholder = document.getElementById('result'),
+		loginButton = document.getElementById('btn-login');
+
+	loginButton.addEventListener('click', function () {
+		login(function (accessToken) {
+			getUserData(accessToken)
+				.then(function (response) {
+					loginButton.style.display = 'none';
+					resultsPlaceholder.innerHTML = template(response);
+				});
+		});
+		console.log("i was here");
+	});
+
 })();
